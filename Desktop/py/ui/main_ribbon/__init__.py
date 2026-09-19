927d143a8b5f4ea1cedf66a36cddeddfa175f9e4
mushket-re0ah <mushket@mushket.localdomain>
2026-09-19 17:41:06 +0300

    overflow_layout in libs now

--- Desktop/py/ui/main_ribbon/__init__.py
+++ Desktop/py/ui/main_ribbon/__init__.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from ui.components.mdi_window import MDIWindow
 from libs.uix.button import ImageToggleButton, HoverToggleButton
-from ui.components.overflow_layout import OverflowLayout
+from libs.uix.overflow_layout import OverflowLayout
 from kivy.properties import ObjectProperty, BooleanProperty, ListProperty
 from kivy.clock import Clock
 from typing import List, Optional
